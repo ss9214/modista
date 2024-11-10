16eefdedf0a34bc4b988ba6892451c3141b537c0
siddarvind <sarvind@umass.edu>
2024-11-09 22:40:07 -0500

    updated the UI for the Muse Home page

--- backend/modista_django/settings.py
+++ backend/modista_django/settings.py
@@ -27,7 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+CSRF_COOKIE_NAME = "csrftoken"
+CSRF_COOKIE_SAMESITE = 'None'  # For cross-origin requests (important if your app and backend are on different domains)
+CSRF_COOKIE_SECURE = True  # If you're using HTTPS
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = "modista_django.urls"
@@ -69,6 +73,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "modista_django.wsgi.application"
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',  # Flutter web app
+    'http://127.0.0.1:8000',  # Django server if needed
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
